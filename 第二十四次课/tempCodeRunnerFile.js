67a61a89704bf0618d281cd5d6f67287e828856c
libo13970401754 <2811350089@qq.com>
2020-12-29 10:11:24 +0800

    第二十四次课

---
+++ 第二十四次课/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+var pitaya3 = {}
+console.log(typeof pitaya3)
+var pitaya4 = new Object();
+// 引用类型 class
+// 普通类型 int char double float
+console.log(pitaya3 instanceof Object)
+console.log(pitaya4 instanceof Object)
